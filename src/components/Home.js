5a0cabeb0eebec002988e305a7f438fd1ce7e168
maxcore25 <mkorobov2002@mail.ru>
2022-04-07 12:16:55 +0300

    Motion Component Added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
-      <Link to="/base">
-        <button>
-          Create Your Pizza
-        </button>
+    <motion.div className='home container' animate={{}}>
+      <motion.h2 animate={{}}>Welcome to Pizza Joint</motion.h2>
+      <Link to='/base'>
+        <motion.button animate={{}}>Create Your Pizza</motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
