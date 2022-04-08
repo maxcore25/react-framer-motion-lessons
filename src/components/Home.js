030b08adb6e64d80249e462fb09c43e3d7afe14f
maxcore25 <mkorobov2002@mail.ru>
2022-04-08 12:24:21 +0300

    Animations In Home, Header And Base Added

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,11 @@ import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
-    <motion.div className='home container' animate={{}}>
+    <motion.div
+      className='home container'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1, duration: 1.5 }}>
       <motion.h2 animate={{}}>Welcome to Pizza Joint</motion.h2>
       <Link to='/base'>
         <motion.button animate={{}}>Create Your Pizza</motion.button>
