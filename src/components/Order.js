a1a13c5944b5f45e87ab4066e73ba61de3480f02
maxcore25 <mkorobov2002@mail.ru>
2022-04-09 12:45:41 +0300

    Initial Animation In Order.js Added

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: '100vw',
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      delay: 0.5,
+      type: 'spring',
+    },
+  },
+};
 
 const Order = ({ pizza }) => {
   return (
-    <div className="container order">
+    <motion.div
+      className='container order'
+      variants={containerVariants}
+      initial='hidden'
+      animate='visible'>
       <h2>Thank you for your order :)</h2>
       <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
-    </div>
-  )
-}
+      {pizza.toppings.map(topping => (
+        <div key={topping}>{topping}</div>
+      ))}
+    </motion.div>
+  );
+};
 
 export default Order;
