0617445b14d7bf4da2c02d8e804b57ded6bb4f59
maxcore25 <mkorobov2002@mail.ru>
2022-04-09 13:27:56 +0300

    Infinite Button Keyframes Added

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: '0 0 8px #fff',
+    boxShadow: '0 0 8px #fff',
+    transition: {
+      yoyo: Infinity,
+      duration: 0.3,
+    },
+  },
+};
+
 const Home = () => {
   return (
     <motion.div
@@ -9,14 +21,12 @@ const Home = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ delay: 1, duration: 1.5 }}>
-      <motion.h2 animate={{}}>Welcome to Pizza Joint</motion.h2>
+      <motion.h2>Welcome to Pizza Joint</motion.h2>
       <Link to='/base'>
         <motion.button
-          whileHover={{
-            scale: 1.1,
-            textShadow: '0 0 8px #fff',
-            boxShadow: '0 0 8px #fff',
-          }}>
+          variants={buttonVariants}
+          animate='visible'
+          whileHover='hover'>
           Create Your Pizza
         </motion.button>
       </Link>
