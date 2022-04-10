1a804c0ff700e5b9064ca0ac4d0378fa8032b22d
maxcore25 <mkorobov2002@mail.ru>
2022-04-10 18:12:55 +0300

    Modal Stylized

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -19,7 +19,14 @@ const Modal = ({ showModal, setShowModal }) => {
           className='backdrop'
           variants={backdrop}
           initial='hidden'
-          animate='visible'></motion.div>
+          animate='visible'>
+          <motion.div className='modal'>
+            <p>Want to make another pizza?</p>
+            <Link to='/'>
+              <button>Start Again</button>
+            </Link>
+          </motion.div>
+        </motion.div>
       )}
     </AnimatePresence>
   );
