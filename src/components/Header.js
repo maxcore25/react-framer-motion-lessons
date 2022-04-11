a7b01ef1e30a0397b84a4fcb5f208929e291c3fa
maxcore25 <mkorobov2002@mail.ru>
2022-04-11 11:40:05 +0300

    SVG Animated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
+const svgVariants = {
+  hidden: { rotate: -180 },
+  visible: { rotate: 0, transition: { duration: 1 } },
+};
+
+const pathVariants = {
+  hidden: { opacity: 0, pathLength: 0 },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    transition: { duration: 2, ease: 'easeInOut' },
+  },
+};
+
 const Header = () => {
   return (
     <header>
       <div className='logo'>
-        <svg
+        <motion.svg
+          variants={svgVariants}
+          initial='hidden'
+          animate='visible'
           className='pizza-svg'
           xmlns='http://www.w3.org/2000/svg'
           viewBox='0 0 100 100'>
-          <path
+          <motion.path
+            variants={pathVariants}
             fill='none'
             d='M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z'
           />
-          <path fill='none' d='M50 30 L50 -10 C50 -10 90 -10 90 30 Z' />
-        </svg>
+          <motion.path
+            variants={pathVariants}
+            fill='none'
+            d='M50 30 L50 -10 C50 -10 90 -10 90 30 Z'
+          />
+        </motion.svg>
       </div>
       <motion.div
         className='title'
